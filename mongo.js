db1b83aee936bfcc2202541574bd0e68237b41b4
jaime <jaime.molina@mattersupply.co>
2020-03-09 18:14:37 -0500

    Exercise

---
+++ mongo.js
@@ -0,0 +1,16 @@
+/**
+ * Crear una collecion USERS
+ *
+ * que contenga el siguiente Objeto
+ */
+
+const exampleOfUsersCollection = {
+  name: '',
+  surname: '',
+  tel: 123123,
+  hobbies: [''],
+  measures: {
+    height: '',
+    weight: ''
+  }
+}
